51ce1673bedbecf4f84cb4eefb58bc7cd635b048
Dmytro Zhigulin <61688545+DmytroZhigulin@users.noreply.github.com>
2023-03-26 15:58:39 +0300

    fix/modules-to-class-components

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import { 
     Box,
@@ -6,45 +6,56 @@ import {
 } from "@mui/material";
 import QuizItem from "../components/QuizItem";
 
+export default class UsersList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            courses: []
+        }
+    }
 
-export default function UsersList() {
+    componentDidMount() {
+        axios.get("https://63f9f88abeec322c57eae484.mockapi.io/Courses")
+            .then(response => {
+                this.setState({
+                    courses: response.data
+                })
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
-    let [courses, setCourses] = useState([]);
-    useEffect(() => {
-        (async () => {
-            const { data } = await axios.get("https://63f9f88abeec322c57eae484.mockapi.io/Courses");
-            setCourses(data);
-            console.log(data);
-        })();
-    }, []);
-
-  return (
-    <Box sx={{ 
-        flexGrow: 1,
-    }}>
-      <Grid container 
-        spacing={1}
-        sx={{ 
-            minHeight: '100vh',
-            paddingTop: '110px',
-            justifyContent: 'center',
-            backgroundImage: 'linear-gradient(to right bottom, #726cf8, #9a67f2, #ba63e9, #d45edf, #ea5bd4)',
-            
-        }}>
-        {courses.map((course, index) => (
-                <QuizItem 
-                    item xs={8} md={4}
-                    key={index}
-                    course={course.course}
-                    descr={course.description}
-                    about={course.about}
-                    img={course.avatar}
-                    baner={course.baner}
-                    id={index}
-                    margin='20px'
-                />
-            ))} 
-      </Grid>
-    </Box>
-  );
+    render() {
+        return (
+            <Box sx={{ 
+                flexGrow: 1,
+            }}>
+                <Grid container 
+                    spacing={1}
+                    sx={{ 
+                        minHeight: '100vh',
+                        paddingTop: '110px',
+                        justifyContent: 'center',
+                        backgroundImage: 'linear-gradient(to right bottom, #726cf8, #9a67f2, #ba63e9, #d45edf, #ea5bd4)',
+                        
+                    }}>
+                    {this.state.courses.map((course, index) => (
+                            <QuizItem 
+                                item xs={8} md={4}
+                                key={index}
+                                course={course.course}
+                                descr={course.description}
+                                about={course.about}
+                                img={course.avatar}
+                                baner={course.baner}
+                                id={index}
+                                margin='20px'
+                            />
+                        ))} 
+                </Grid>
+            </Box>
+        );
+    }
 }
